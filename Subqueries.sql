ebda12e39fa3c9dfb634be4cbc9b1a950f9bf50f
Marlon Diaz <97621295+mdiaz46@users.noreply.github.com>
2024-08-02 13:20:40 -0700

    Subqueries.sql

--- Subqueries.sql
+++ Subqueries.sql
@@ -1,3 +1,23 @@
-SELECT market_date, market_rain_flag
-FROM farmers_market.market_date_info
-WHERE market_rain_flag = 1
+SELECT 
+   
+ market_date, 
+   
+ customer_id, 
+   
+ vendor_id, 
+   
+ quantity * cost_to_customer_per_qty price
+FROM farmers_market.customer_purchases
+WHERE 
+   
+ market_date IN
+      
+ (
+      
+ SELECT market_date
+      
+ FROM farmers_market.market_date_info
+      
+ WHERE market_rain_flag = 1
+      
+ )
